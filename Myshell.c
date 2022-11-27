42509f8b3af863f057719eb748b8f0324c411795
bahadirsina <72709072+bahadirsina@users.noreply.github.com>
2022-11-27 16:44:46 +0300

    Add files via upload

---
+++ Myshell.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+int main(int argc, char *argv[]) {
+	char arr [100];
+	printf("------- BahadirSina's Myshell -------\n");
+	while(true){
+	printf("myshell>> ");
+	fgets(arr,100,stdin);
+	puts(arr);
+	if()
+	
+	
+	
+	
+	}
+	
+    return 0;
+}
